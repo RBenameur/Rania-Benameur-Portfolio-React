f4f30bb8765b5c4b45fa5df4ef96788dbc878d83
Rania Benameur <raniabenameur97@gmail.com>
2023-02-23 22:27:19 +0000

    injected html for Portfolio component

--- my-react-app/src/components/portfolio/Portfolio.js
+++ my-react-app/src/components/portfolio/Portfolio.js
@@ -3,7 +3,46 @@ function Portfolio(){
 
     return (
         <>
-        <h1>Portfolio</h1>
+        <section class="page-section" id="work">
+            <h2>work.</h2>
+            <div class="card-container">
+                <div class=" card card1">
+                    <i class="fas fa-chevron-down fa-3x"></i>
+                    <div class="card-text">
+                        <h3>Recipedia</h3>
+                        <h5>HTML CSS JavaScript JQuery Bootstraps</h5>
+                        <a href="https://github.com/o1ujok3/Recipedia" target="_blank"><p>visit github repo</p></a>
+                    </div>
+                </div>
+
+                <div class="card card2">
+                    <i class="fas fa-chevron-down fa-3x"></i>
+                    <div class="card-text">
+                        <h3>Weather Dashboard</h3>
+                        <h5> HTML CSS JavaScript JQuery Moment.js Bootstraps</h5>
+                        <a href="https://github.com/RBenameur/Weather-Dashboard" target="_blank"><p>visit github repo</p></a>
+                    </div>
+                </div>
+
+                <div class=" card card3">
+                    <i class="fas fa-chevron-down fa-3x"></i>
+                    <div class="card-text">
+                        <h3>Daily Planner</h3>
+                        <h5> HTML CSS JavaScript Moment.js JQuery Bootstraps</h5>
+                        <a href="https://github.com/RBenameur/Daily-Planner-App" target="_blank"><p>visit github repo</p></a>
+                    </div>
+                </div>
+
+                <div class=" card card4">
+                    <i class="fas fa-chevron-down fa-3x"></i>
+                    <div class="card-text">
+                        <h3>Javascript Quiz</h3>
+                        <h5>HTML CSS Javascript</h5>
+                        <a href="https://github.com/RBenameur/javascript-quiz" target="_blank"><p>visit github repo</p></a>
+                    </div>
+                </div>
+             </div>
+        </section>
         </>
     );
 }
