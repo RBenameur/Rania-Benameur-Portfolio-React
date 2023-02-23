a2b22ff3ed77f8b59c824a4dd4bb45fe476bbc8a
Rania Benameur <raniabenameur97@gmail.com>
2023-02-23 22:29:20 +0000

    inject html for Contact section

--- my-react-app/src/components/contact/Contact.js
+++ my-react-app/src/components/contact/Contact.js
@@ -3,7 +3,13 @@ function Contact(){
 
     return (
         <>
-        <h1>Contact</h1>
+            <section class="page-section contact" id="contact">
+                <h2>contact me.</h2>
+                <div class="contact-container">
+                    <p>+44 (0) 79 44 47 20 68</p>
+                    <p>raniabenameur97@gmail.com</p>
+                </div>
+            </section>
         </>
     );
 }
