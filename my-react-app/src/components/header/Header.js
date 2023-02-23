cb190b0e912b7c981f9532c15d964ab671433edc
Rania Benameur <raniabenameur97@gmail.com>
2023-02-23 22:16:36 +0000

    added HTML for header component

--- my-react-app/src/components/header/Header.js
+++ my-react-app/src/components/header/Header.js
@@ -3,7 +3,24 @@ function Header(){
 
     return (
         <>
-        <h1>Header section</h1>
+        <header>
+            <nav>
+                <div id="logo">
+                    <h1>_Rania_Benameur_</h1>
+                </div>
+                <div id="navigation"> 
+                    <a href="#about">about me.</a>
+                    <a href="#work">work.</a>
+                    <a href="#contact">contact.</a>
+                </div>
+            </nav>
+            <div class="hero-container">
+                <a href="#about">
+                    <p>icon</p>
+                    {/* <i class="fas fa-chevron-down fa-3x"></i> */}
+                    </a>
+            </div>
+        </header>
         </>
     );
 }
