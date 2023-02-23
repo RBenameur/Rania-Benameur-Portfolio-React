0f30e72809293c1b5af145a31c2ca9f4c4742d2f
Rania Benameur <raniabenameur97@gmail.com>
2023-02-23 22:34:07 +0000

    added html for footer section

--- my-react-app/src/components/footer/Footer.js
+++ my-react-app/src/components/footer/Footer.js
@@ -3,7 +3,17 @@ function Footer(){
 
     return (
         <>
-        <h1>Footer section</h1>
+          <footer>
+                <div id="social-media">
+                    <a href="https://www.linkedin.com/in/rania-b-7992a419b/" target="_blank">
+                        {/* <i class="fa fa-linkedin"></i> */}
+                        </a>
+                    <a href="https://github.com/RBenameur" target="_blank">
+                        {/* <i class="fa fa-github"></i> */}
+                        </a>
+                </div>
+                <p id="copyright"> &copy; Rania Benameur 2023</p>
+            </footer> 
         </>
     );
 }
