0478b6fc1f1aeb81d5130242bd4ae9c18c454550
Rania Benameur <raniabenameur97@gmail.com>
2023-02-24 21:15:22 +0000

    added link to download CV

--- my-react-app/src/components/contact/Contact.js
+++ my-react-app/src/components/contact/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Element } from 'react-scroll';
 import "./contact.css";
+import {Link} from 'react-router-dom';
 
 function Contact(){
 
@@ -12,6 +13,7 @@ function Contact(){
                 <div className="contact-container">
                     <p>+44 (0) 79 44 47 20 68</p>
                     <p>raniabenameur97@gmail.com</p>
+                    <Link to="../../CV/Rania_BENAMEUR_CV.pdf" target="_blank" download rel="noopener noreferrer"><p>Download CV</p></Link>
                 </div>
             </section>
         </Element>
