194718912a094db5212cb239c866389fda40cce6
Rania Benameur <raniabenameur97@gmail.com>
2023-02-24 01:36:00 +0000

    added CSS styling for header section

--- my-react-app/src/components/header/Header.js
+++ my-react-app/src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./header.css";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronDown  } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,14 +18,14 @@ function Header(){
                     <h1>_Rania_Benameur_</h1>
                 </div>
                 <div id="navigation"> 
-                    <Link to="about">about me.</Link>
-                    <Link to="work">work.</Link>
-                    <Link to="contact">contact.</Link>
+                    <Link to="about" className="link-class">about me.</Link>
+                    <Link to="work" className="link-class">work.</Link>
+                    <Link to="contact" className="link-class">contact.</Link>
                 </div>
             </nav>
             <div className="hero-container">
                 <Link to="about">
-                    <FontAwesomeIcon icon={faChevronDown} />
+                    <FontAwesomeIcon icon={faChevronDown} className="icon" />
                 </Link>
             </div>
         </header>
