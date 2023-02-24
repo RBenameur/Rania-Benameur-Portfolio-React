b86a27fa4484c80a47a835825ceefaf6d35d147d
Rania Benameur <raniabenameur97@gmail.com>
2023-02-24 20:44:05 +0000

    props passed to portfolio component from the portfolio page

--- my-react-app/src/components/portfolio/Portfolio.js
+++ my-react-app/src/components/portfolio/Portfolio.js
@@ -3,14 +3,26 @@ import "./portfolio.css";
 // import { Element } from 'react-scroll';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import portfolioData from "../../data/projectData.json";
 
-function Portfolio(){
+function Portfolio(props){
+        
+    const dataObj = portfolioData.data;
+
+    const index = props.injectData;
 
     return (
         <>
-            <div className=" card card1">
+        <div className={"card" + index}>
+        <FontAwesomeIcon icon={faChevronDown} className="icon" />
+            <div className="card-text">
+                <h3>{dataObj[index].name}</h3>
+                <h5>HTML CSS Javascript</h5>
+                <a href="https://github.com/RBenameur/javascript-quiz" target="_blank" rel="noopener noreferrer"><p>visit github repo</p></a>
+            </div>
+        </div>
+            {/* <div className=" card card1">
             <FontAwesomeIcon icon={faChevronDown} className="icon" />
-                {/* <i className="fas fa-chevron-down fa-3x"></i> */}
                 <div className="card-text">
                     <h3>Recipedia</h3>
                     <h5>HTML CSS JavaScript JQuery Bootstraps</h5>
@@ -20,7 +32,6 @@ function Portfolio(){
 
             <div className="card card2">
             <FontAwesomeIcon icon={faChevronDown} className="icon" />
-                {/* <i className="fas fa-chevron-down fa-3x"></i> */}
                 <div className="card-text">
                     <h3>Weather Dashboard</h3>
                     <h5> HTML CSS JavaScript JQuery Moment.js Bootstraps</h5>
@@ -30,7 +41,6 @@ function Portfolio(){
 
             <div className=" card card3">
             <FontAwesomeIcon icon={faChevronDown} className="icon" />
-                {/* <i className="fas fa-chevron-down fa-3x"></i> */}
                 <div className="card-text">
                     <h3>Daily Planner</h3>
                     <h5> HTML CSS JavaScript Moment.js JQuery Bootstraps</h5>
@@ -40,13 +50,12 @@ function Portfolio(){
 
             <div className=" card card4">
             <FontAwesomeIcon icon={faChevronDown} className="icon" />
-                {/* <i className="fas fa-chevron-down fa-3x"></i> */}
                 <div className="card-text">
                     <h3>Javascript Quiz</h3>
                     <h5>HTML CSS Javascript</h5>
                     <a href="https://github.com/RBenameur/javascript-quiz" target="_blank" rel="noopener noreferrer"><p>visit github repo</p></a>
                 </div>
-            </div>
+            </div> */}
         </>
     );
 }
--- my-react-app/src/pages/portfolio/Portfolio.js
+++ my-react-app/src/pages/portfolio/Portfolio.js
@@ -2,14 +2,20 @@ import React from "react";
 import './portfolio.css';
 import Portfolio from "../../components/portfolio/Portfolio";
 
+
+
 function PortfolioPage() {
+
+
+
+    // console.log(dataList);
 
     return (
         <>
             <section className="page-section" id="work">
                 <h2>work.</h2>
                 <div className="card-container">
-                    <Portfolio />
+                    <Portfolio injectData="0" />
                 </div>
             </section>
         </>
