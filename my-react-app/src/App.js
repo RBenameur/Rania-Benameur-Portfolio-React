cc8dd6d74d250c26f55dc16aa1f3361828e6b754
Rania Benameur <raniabenameur97@gmail.com>
2023-02-24 12:46:28 +0000

    refactored header and added routes into App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
 import Header from "./components/header/Header";
-import About from "./components/about/About";
-import Portfolio from "./components/portfolio/Portfolio";
 import Footer from "./components/footer/Footer";
-import Contact from "./components/contact/Contact"
+// import About from "./components/about/About";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Contact from "./components/contact/Contact"
+
+import Home from "./pages/home/Home";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/porfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
 
-      <About />
+      {/* <About />
 
       <Portfolio />
 
-      <Contact />
+      <Contact /> */}
 
       <Footer />
 
--- my-react-app/src/components/header/Header.js
+++ my-react-app/src/components/header/Header.js
@@ -3,11 +3,10 @@ import "./header.css";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronDown  } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { animateScroll, scrollTo } from 'react-scroll';
 import { Link } from 'react-scroll';
+import { NavLink } from "react-router-dom";
 
 library.add(faChevronDown);
-console.log(library);
 
 function Header(){
 
@@ -16,12 +15,13 @@ function Header(){
         <header>
             <nav>
                 <div id="logo">
-                    <h1>_Rania_Benameur_</h1>
+                    <h1>Rania Benameur</h1>
                 </div>
-                <div id="navigation"> 
+                <div id="navigation">
+                    <NavLink to="/" className="link-class">home.</NavLink> 
                     <Link to="about" className="link-class">about me.</Link>
-                    <Link to="work" className="link-class">work.</Link>
-                    <Link to="contact" className="link-class">contact.</Link>
+                    <NavLink to="/work" className="link-class">work.</NavLink>
+                    <NavLink to="/contact" className="link-class">contact.</NavLink>
                 </div>
             </nav>
             <div className="hero-container">
