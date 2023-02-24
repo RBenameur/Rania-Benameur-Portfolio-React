61e31fc0b45d73425d145098ec9b5a9a789df69c
Rania Benameur <raniabenameur97@gmail.com>
2023-02-24 18:18:18 +0000

    refactored hero styling

--- my-react-app/src/components/Hero/Hero.js
+++ my-react-app/src/components/Hero/Hero.js
@@ -4,7 +4,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronDown  } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from 'react-scroll';
-// import { NavLink } from "react-router-dom";
 
 library.add(faChevronDown);
 
@@ -12,13 +11,11 @@ function Hero() {
 
     return (
         <>
-        <header>
-            <div className="hero-container">
-                <Link to="about">
-                    <FontAwesomeIcon icon={faChevronDown} className="icon" />
-                </Link>
-            </div>
-        </header>
+        <div className="hero-container">
+            <Link to="about">
+                <FontAwesomeIcon icon={faChevronDown} className="icon" />
+            </Link>
+        </div>
         </>
     );
 }
